e9b9e4cf230edf26873982bbda05a58a8b3aa3d4
jjoshua2 <jjoshua2@gmail.com>
2018-07-02 16:31:11 -0400

    Update node.cc
    support BLACK_WON terminal state

--- src/mcts/node.cc
+++ src/mcts/node.cc
@@ -275,7 +275,13 @@ Move Node::GetMove(bool flip) const {
 
 void Node::MakeTerminal(GameResult result) {
   is_terminal_ = true;
-  q_ = (result == GameResult::DRAW) ? 0.0f : 1.0f;
+  if (result == GameResult::DRAW) {
+    q_ = 0.0f;
+  else if (result == GameResult::WHITE_WON) {
+    q_ = 1.0f;
+  } else {
+    q_ = -1.0f;
+  }
 }
 
 bool Node::TryStartScoreUpdate() {
